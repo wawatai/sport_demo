fb04cf6bcc4c8b8979d5bc248f7d5a12639d6c1b
wawatai <wataiPublic@gmail.com>
2022-09-23 17:18:25 +0800

    111-09-23
    +自動向左輪播

--- js/js.jsx
+++ js/js.jsx
@@ -3,6 +3,8 @@ $(function(){
     $('.mutltiple-itme').slick({
         slidesToShow: 5,
         slidesToScroll: 1,
+        autoplay: true,
+		autoplaySpeed: 1800,
         arrows: true,
         dots: false,
         centerMode: true,
